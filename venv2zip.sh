a0f1821257454a33ba77ad366fc4c77746a9089f
Tamaki <83083937+tamaki380@users.noreply.github.com>
2024-06-23 01:41:14 +0900

    v1パッケージ

--- venv2zip.sh
+++ venv2zip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -r ex-library
 pip freeze > requirements.txt
 pip3 install -r requirements.txt -t ex-library
 # Compress-Archive -Path .\ex-library\* -DestinationPath .\ex-library.zip
